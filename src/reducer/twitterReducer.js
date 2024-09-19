b00865430e0da32b18730628c951096e218ddfc8
EKil <loreleyko@gmail.com>
2024-09-19 07:40:01 +0200

    First commit

--- src/reducer/twitterReducer.js
+++ src/reducer/twitterReducer.js
@@ -14,32 +14,16 @@ const initialState = {
 export const twitterReducer = (state = initialState, action) => {
   switch (action.type) {
     case CHANGE_AVATAR:
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          avatar: action.payload || state.user.avatar,
-        },
-      };
+      return { ...state, user: {...state.user, avatar: action.payload || state.user.avatar}};
 
     case CHANGE_NAME:
-      return {
-        ...state,
-        user: {
-          ...state.user,
-          name: action.payload || state.user.name,
-        },
-      };
+      return { ...state, user: { ...state.user, name: action.payload || state.user.name}};
 
     case CHANGE_STATS:
       const { statsType, sum } = action.payload;
-      return {
-        ...state,
-        stats: {
-          ...state.stats,
-          [statsType]: Math.max(0, state.stats[statsType] + sum),
-        },
-      };
+      let res = state.stats[statsType] + sum;
+      res = res < 0 ? 0 : res;
+      return { ...state, stats: {...state.stats, [statsType]: res}};
 
     default:
       return state;
